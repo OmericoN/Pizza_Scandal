74ca9b7310ada48c2ce39f3845238485bdcf8d6f
Omer <96557499+OmericoN@users.noreply.github.com>
2025-09-15 12:51:10 +0200

    schema cleaned

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "12345"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    
+
+if __name__ = "__main__":
+    app = create_app()
+    app.run(debug=True)
