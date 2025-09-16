4eb8582950d58cd4a1624517c83e68a66a1aacd3
Omer <96557499+OmericoN@users.noreply.github.com>
2025-09-16 20:08:53 +0200

    Supabase has been set up, database is now being hosted online!!!

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
+from models import db
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
-    app.secret_key = "12345"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.secret_key = os.environ.get("SECRET_KEY", "dev-key-production")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI") #Uses a .env to hide password (I'll help you set it up Raul)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
-if __name__ = "__main__":
+    @app.route("/")
+    def index():
+        return ("<p>Pizzeria app. Supabase connection is working</p>")
+    
+    return app
+
+if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
